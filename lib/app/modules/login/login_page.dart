36327a0ee9a1307c5094c6653c617990393791ac
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-08-02 15:55:40 -0300

    Tela de Login

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  const LoginPage({ super.key });
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Login Page'),),
-           body: Container(),
-       );
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0XFF0092B9),
+            Color(0XFF0167B2),
+          ],
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(height: screenSize.height * 0.2),
+              SignInButton(
+                Buttons.Google,
+                onPressed: () {},
+                text: 'Continue com o Google',
+                padding: const EdgeInsets.all(4),
+                shape: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(30)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
