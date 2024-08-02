11f1535195508582481d09da4e36a0c2704de50c
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-08-02 15:50:03 -0300

    Interceptor FirebaseAuth

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -7,7 +7,7 @@ class LoginPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
+           appBar: AppBar(title: const Text('Login Page'),),
            body: Container(),
        );
   }
