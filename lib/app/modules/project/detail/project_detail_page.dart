5eb5197c631fceea2388759ac3a4028094ff26c2
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-08-06 13:30:19 -0300

    Criar tela de Detalhe de Projeto

---
+++ lib/app/modules/project/detail/project_detail_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:job_timer/app/modules/project/detail/widgets/project_detail_appbar.dart';
+import 'package:job_timer/app/modules/project/detail/widgets/project_pie_chart.dart';
+import 'package:job_timer/app/modules/project/detail/widgets/project_task_tile.dart';
+
+class ProjectDetailPage extends StatelessWidget {
+  const ProjectDetailPage({super.key});
+
+  get icon => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          ProjectDetailAppbar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: ProjectPieChart(),
+            ),
+            const ProjectTaskTile(),
+            const ProjectTaskTile(),
+          ])),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    icon: const Icon(Icons.check_circle),
+                    label: const Text("Finalizar Projeto")),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
